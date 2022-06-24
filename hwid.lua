31e8555945d416b7f1e74949740ba2ce19dafe08
vinhuchi <88415185+vinhuchi@users.noreply.github.com>
2022-06-24 13:31:48 +0700

    Create hwid.lua

---
+++ hwid.lua
@@ -0,0 +1,12 @@
+local req = (syn and syn.request) or request or http_request
+local body = req({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
+local decoded = game:GetService('HttpService'):JSONDecode(body)
+local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint", "Krnl-Hwid",'shadow_hardware',"Flux-Fingerprint"};
+local hwid3 = "";
+for i, v in next, hwid_list do
+    if decoded.headers[v] then
+        hwid3 = decoded.headers[v];
+        break
+    end
+end
+return hwid3
