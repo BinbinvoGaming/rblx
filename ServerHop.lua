976a56e0803f40ef73e5f65a0237d0e1b4d3a95c
vinhuchi <88415185+vinhuchi@users.noreply.github.com>
2022-06-24 16:31:00 +0700

    Create ServerHop.lua

---
+++ ServerHop.lua
@@ -0,0 +1,35 @@
+--Charwar
+local request = request or http_request or syn.request
+local foundAnything = ""
+function TPReturner()
+    local Site;
+    if foundAnything == "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        ID = tostring(v.id)
+        pcall(function()
+            if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                SH(ID)
+            end
+        end)
+    end
+end
+function Teleport()
+    while wait() do
+        pcall(function()
+            TPReturner()
+            if foundAnything ~= "" then
+                TPReturner()
+            end
+        end)
+    end
+end
+Teleport()
