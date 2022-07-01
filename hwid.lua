4240a4c86fe400f7f7bfd95c567a736b79b278c1
vinhuchi <88415185+vinhuchi@users.noreply.github.com>
2022-07-01 18:25:36 +0700

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,7 +1,7 @@
 local req = (syn and syn.request) or request or http_request
 local body = req({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
 local decoded = game:GetService('HttpService'):JSONDecode(body)
-local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint", "Krnl-Hwid",'shadow_hardware',"Flux-Fingerprint"};
+local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint", "Krnl-Hwid",'shadow_hardware',"Flux-Fingerprint","Wrd-Fingerprint"};
 local hwid3 = "";
 for i, v in next, hwid_list do
     if decoded.headers[v] then
